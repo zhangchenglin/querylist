5ce7a52d55687c95818cd0f59a5b87e6b6824659
zhangchenglin <469946668@qq.com>
2021-01-22 17:48:10 +0800

    -

--- index-0.php
+++ index-0.php
@@ -7,22 +7,23 @@ require_once dirname(__FILE__) . '/vendor/autoload.php';
 
 $url = 'http://www.mca.gov.cn/article/sj/xzqh/2020/2020/202101041104.html';
 
-$find = 'tr:gt(2) td:eq(2)';
-
-
-
-
-
-
-
+$find_code = 'tr:gt(1) td:eq(2)';
+$find_name = 'tr:gt(2) td:eq(2)';
+$find_parent = 'tr:gt(3) td:eq(2)';
+$rules = [
+    'code' => [$find_code, 'text'],
+    'name' => [$find_name, 'text'],
+    'parent' => [$find_parent, 'text'],
+];
 
 $data = (new QueryList)
     ->get($url)
-    ->find($find)
-    ->texts()
-    ->all();
+    ->rules($rules)
+    ->range('')
+    ->queryData();
 
 // ---------------------------------------------------------------------------------------------------------------------
+
 echo '<br>';
 echo $url;
 echo '<hr><br>';
