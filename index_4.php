a5ffdcf9c5d87c70f01a45a6ab0e8a05696daf56
zhangchenglin <469946668@qq.com>
2021-01-22 11:37:01 +0800

    -

--- index_4.php
+++ index_4.php
@@ -28,7 +28,7 @@ $html = <<<STR
     </div>
 STR;
 
-$data = QueryList::html($html)
+$data = (new QueryList)->html($html)
     ->rules(
         [
             'title' => ['h3', 'text'],
@@ -39,7 +39,7 @@ $data = QueryList::html($html)
     ->queryData(function ($item) {
         // 注意这里的QueryList对象与上面的QueryList对象是同一个对象
         // 所以这里要重置range()参数，否则会共用前面的range()参数，导致出现采集不到结果的诡异现象
-        $item['list'] = QueryList::html($item['list'])
+        $item['list'] = (new QueryList)->html($item['list'])
             ->rules(
                 [
                     'item' => ['.item', 'text']
@@ -50,6 +50,17 @@ $data = QueryList::html($html)
         return $item;
     });
 
+?>
+
+<html lang="zh_CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>递归多级采集</title>
+</head>
+<body>
+<?php
 
 echo '<br>';
 echo $html;
@@ -60,3 +71,7 @@ echo '<br>';
 echo '<hr>';
 echo json_encode($data);
 //echo '<br><br>';
+
+?>
+</body>
+</html>
